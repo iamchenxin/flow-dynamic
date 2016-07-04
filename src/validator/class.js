d925439f4473ade3b69a7514705c8f3fa08c66c1
iamchenxin <iamchenxin@gmail.com>
2016-07-04 09:57:00 -0600

    custom error info,instead of error msg of `instanceof`

--- src/validator/class.js
+++ src/validator/class.js
@@ -5,15 +5,14 @@
 import {RunTimeCheckE, ePrint} from '../definition/def.js';
 
 export function isInstanceof<Type>(v:mixed, _class:Class<Type>):Type {
-  if (typeof _class === 'function') {
-    if ( v instanceof _class) {
-      return v;
-    } else {
-      throw new RunTimeCheckE(
-        `value:(${ePrint(v)}) should be instanceof ${_class.name}`);
-    }
-  } else {
+  if ( typeof _class !== 'function' ) {
     throw new RunTimeCheckE(
       `The Type(${ePrint(_class)}) you passed in is not a class!`);
+  }
+  if ( v instanceof _class) {
+    return v;
+  } else {
+    throw new RunTimeCheckE(
+      `value:(${ePrint(v)}) should be instanceof ${_class.name}`);
   }
 }
